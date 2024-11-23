7ca2e079c07356b1b128c036b35d218479792111
cristian gil <45280035+cristiangil28@users.noreply.github.com>
2024-11-22 23:48:20 -0500

    Se adiciona la caracteristica crossorigin

--- src/main/java/com/cristian/controller/ClienteController.java
+++ src/main/java/com/cristian/controller/ClienteController.java
@@ -9,6 +9,7 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +20,7 @@ import com.cristian.service.ClienteService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ClienteController {
 	
 	@Autowired
