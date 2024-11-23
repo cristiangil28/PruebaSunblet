ab102755822f8ef9ff52c573454d3fc2a2d6b68a
cristian gil <45280035+cristiangil28@users.noreply.github.com>
2024-11-23 13:53:43 -0500

    refactorizando

--- src/main/java/com/cristian/controller/ClienteController.java
+++ src/main/java/com/cristian/controller/ClienteController.java
@@ -4,8 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import java.util.Optional;
-
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- src/test/java/com/cristian/ApiSunbletApplicationTests.java
+++ src/test/java/com/cristian/ApiSunbletApplicationTests.java
@@ -1,27 +1,16 @@
 package com.cristian;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.util.LinkedMultiValueMap;
 
-import com.cristian.model.Cliente;
 import com.cristian.service.ClienteService;
 
 @SpringBootTest
