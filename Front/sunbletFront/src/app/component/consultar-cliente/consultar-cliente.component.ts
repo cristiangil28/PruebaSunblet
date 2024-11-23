85aa25e75a97c7b8cb891517737c2bb83354feed
cristian gil <45280035+cristiangil28@users.noreply.github.com>
2024-11-23 01:34:56 -0500

    Mostrando los datos del cliente

--- Front/sunbletFront/src/app/component/consultar-cliente/consultar-cliente.component.ts
+++ Front/sunbletFront/src/app/component/consultar-cliente/consultar-cliente.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ClienteService } from '../../service/cliente/cliente.service';
+import { Router } from '@angular/router';
+import { Cliente } from '../../model/cliente/cliente';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-consultar-cliente',
@@ -12,16 +15,46 @@ import { ClienteService } from '../../service/cliente/cliente.service';
 export class ConsultarClienteComponent {
   tipoDocumento : string = '';
   numeroDocumento : string = '';
-  constructor(private clienteService: ClienteService){
+  resultado : boolean = false;
+  cliente: Cliente = new Cliente();
+  constructor(private clienteService: ClienteService,private router: Router){
 
   }
   consultarCliente(){
+    if(this.numeroDocumento == '' || this.tipoDocumento == ''){
+      Swal.fire({
+        title: 'Error!',
+        text: 'Faltan Datos',
+        icon: 'error',
+        confirmButtonText: 'Ok'
+      })
+    }
     if(this.numeroDocumento != '' && this.tipoDocumento != ''){
-      this.clienteService.consultarCliente(this.tipoDocumento, this.numeroDocumento).subscribe(data =>{
+      this.clienteService.consultarCliente(this.tipoDocumento, this.numeroDocumento).subscribe({ next: (data) =>{
         if(data != null){
-          console.log(data);
+          const datosJson = JSON.parse(JSON.stringify(data));
+          Swal.fire({
+            title: 'Correcto',
+            text: 'Dato correcto',
+            icon: 'success',
+            confirmButtonText: 'Ok'
+          })
+          this.cliente.primerNombre = datosJson.primerNombre;
+          this.cliente.segundoNombre = datosJson.segundoNombre;
+          this.cliente.primerApellido = datosJson.primerApellido;
+          this.cliente.segundoApellido = datosJson.segundoApellido;
+          this.cliente.telefono = datosJson.telefono;
+          this.cliente.direccion = datosJson.direccion;
+          this.cliente.ciudadResidencia = datosJson.ciudadResidencia;
+          this.resultado = true;
         }
-      });
+      },error:(error) =>{ this.resultado = false;
+      Swal.fire({
+        title: 'Error!',
+        text: 'Dato no existe',
+        icon: 'error',
+        confirmButtonText: 'Ok'
+      })}});
     }
   }
 }
